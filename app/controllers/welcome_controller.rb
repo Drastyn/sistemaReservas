e4469b58f32b6b495de4a96f40a8b0166dac43f6
RonSour <joaquin.dinamarcad@utem.cl>
2018-12-04 16:48:50 -0300

    se actualiza vista welcome, se agregan validaciones de cliente y tarifa

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   def index
     @face = 'https://www.facebook.com/hostelmyhouse/'
     @booking = 'https://www.booking.com/hotel/cl/my-house-hostel.es.html'
-    @nosotros = 'http://localhost:3000/nosotros/index'
+    @instagram = 'https://www.instagram.com/hostalmyhouse/'
     @direccion = 'Bellavista 115, 2580067 Viña del Mar, Chile'
     @fono = 'Aqui va el telefono'
   end
--- app/models/tarifa.rb
+++ app/models/tarifa.rb
@@ -1,3 +1,12 @@
 class Tarifa < ApplicationRecord
   has_many :habitacions
+
+  validates :preciotarifa , numericality: {only_integer: true, message: "ingrese solo numeros"}, presence: {message: "campo en blanco"}
+  validate :mayor_cero
+
+  def mayor_cero
+    if preciotarifa < 10000
+      errors.add(:preciotarifa, "debe ingresar un precio adecuado")
+    end
+  end
 end
