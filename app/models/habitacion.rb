a6d4e1a506fb33f7f08ecd1a44835343418ffe11
vitokooo <vgongora.92@gmail.com>
2018-12-05 21:23:46 -0300

    Validaciones Habitacion y Reserva

--- app/models/habitacion.rb
+++ app/models/habitacion.rb
@@ -1,3 +1,7 @@
 class Habitacion < ApplicationRecord
   has_many :reservas
+
+  validates :descripcionHabitacion, length: {in: 7..200, message: => "Favor ingrese una descripcion de la habitacion"}, presence: {message: "campo en blanco"}
+  validates :tipoHabitacion, length: {in: 7..50, message: => "Favor ingrese el tipo de habitacion"}, presence: {message: "campo en blanco"}
+  validates :numeroPersonas, numericality: {only_integer: true, message: "Favor ingrese la cantidad de personas que tiene la habitacion"}, presence: {message: "campo en blanco"}
 end
