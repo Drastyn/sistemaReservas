000e571a67e9b2ffe4de117b3c9b0bcdc3edbab9
Yesseniaaa <yessenia.allende.a@gmail.com>
2018-12-26 21:41:19 -0300

    Se modifico las validaciones de reserva y se creo el controlador reserva ademas de las vistas

---
+++ app/controllers/reservas_controller.rb
@@ -0,0 +1,40 @@
+class ReservasController < ApplicationController
+  #GET /reservas
+  def index
+    @reservas = Reserva.all
+  end
+  #GET /reservas/:id
+  def show
+    @reservas = Reserva.find(params[:id])
+  end
+
+  #GET /reservas
+  def new
+    @reservas = Reserva.new
+  end
+  #POST /clientes
+  def create
+    @reservas = Reserva.new(fechaIngreso: params[:reserva][:fechaIngreso],
+                            fechaSalida: params[:reserva][:fechaSalida],
+                            cantidadPersonas: params[:reserva][:cantidadPersonas],
+                            cantidadHabitaciones: params[:reserva][:cantidadHabitaciones],
+                            estadoReserva: params[:reserva][:estadoReserva],
+                            precioReserva: params[:reserva][:precioReserva])
+    @reservas.save
+    redirect_to @reservas
+  end
+
+  def destroy
+
+  end
+
+  #PUT /reservas/:id
+  def update
+
+  end
+
+  def edit
+
+  end
+
+end
